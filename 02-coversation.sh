7caf1fdaddea3b7f8fdb9651f830b30f6c4172ad
dharsha27 <dharshavardhan27@gmail.com>
2026-06-09 21:18:51 +0530

    second script

---
+++ 02-coversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+    echo "Trump:Hey Iran i am going to blast you "
+    echo "Iran: Yes please , you are welcome"
+    echo "Trum: I am Serious"
+    echo "Iran: Yeah we know , we are also serios"
