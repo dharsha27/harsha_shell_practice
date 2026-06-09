033678d0dd0ec39a1c5a3951c3de6ceb50ee8469
dharsha27 <dharshavardhan27@gmail.com>
2026-06-09 21:42:50 +0530

    second script

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,4 +3,11 @@
 echo "Please enter your Username::"
 read USER_NAME
 
+
+
 echo "Username is $USER_NAME"
+
+echo "PLease enter your password"
+read -s PASSWORD
+
+echo "This is your password:$PASSWORD"
