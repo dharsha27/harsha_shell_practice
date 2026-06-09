b3727d2a455d443bca4a8859edddda0caa9bf155
dharsha27 <dharshavardhan27@gmail.com>
2026-06-09 21:33:30 +0530

    second script

--- 02-coversation.sh
+++ 02-coversation.sh
@@ -4,5 +4,5 @@
 
     echo "$Person1:Hey $Person2 i am going to blast you "
     echo "$Person2: Yes please , you are welcome"
-    echo "$$Person1: I am Serious"
-    echo "$$Person2: Yeah we know , we are also serios"
+    echo "$Person1: I am Serious"
+    echo "$Person2: Yeah we know , we are also serios"
