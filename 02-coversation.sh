a60fd7d911e07a2362393de9c185628b4e15efb1
dharsha27 <dharshavardhan27@gmail.com>
2026-06-09 21:32:35 +0530

    second script

--- 02-coversation.sh
+++ 02-coversation.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-    USPRESIDENT=$1
-    IRANPRESIDNET=$2
+    Person1=$1
+    Person2=$2
 
-    echo "$USPRESIDENT:Hey Iran i am going to blast you "
-    echo "$IRANPRESIDNET: Yes please , you are welcome"
-    echo "$USPRESIDENT: I am Serious"
-    echo "$IRANPRESIDNET: Yeah we know , we are also serios"
+    echo "$Person1:Hey $Person2 i am going to blast you "
+    echo "$Person2: Yes please , you are welcome"
+    echo "$$Person1: I am Serious"
+    echo "$$Person2: Yeah we know , we are also serios"
