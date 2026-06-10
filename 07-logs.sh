6eae4ced37ceada5e45e02b7ba15187a04129d1c
dharsha27 <dharshavardhan27@gmail.com>
2026-06-10 15:35:26 +0530

    changes were made

---
+++ 07-logs.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_DIR=/home/ec2-user/shell-lgs
+LOGS_FILE="$LOGS_DIR/$0.log"
+
+
+if [ $USERID -ne 0 ]; then 
+   echo "Please run the script with root access..."
+#    exit 1
+fi
+
+VALIDATE(){
+    if [ $2 -ne 0 ]; then 
+   
+          echo "Installing $1 is ....FAILED"
+          exit 1
+
+    else
+          echo "Installing $1 is ....SUCCESS"
+    fi
+
+}
+
+dnf list installed mysql &>> $LOGS_FILE
+echo "Installing my sql"
+if [ $? -eq 0 ]; then 
+   echo "MYSQL is Alreaady Installed .. please skip"
+
+else  
+    echo "Installing Mysql " 
+    dnf install mysql -y
+    VALIDATE Mysql $?
+fi
+
+
+
+dnf list installed nginx &>> $LOGS_FILE
+echo "Installing my sql"
+if [ $? -eq 0 ]; then 
+   echo "NGINX is Alreaady Installed .. please skip"
+
+else  
+    echo "Installing NGINX " 
+    dnf install nginx -y &>> $LOGS_FILE
+    VALIDATE Mysql $?
+fi
