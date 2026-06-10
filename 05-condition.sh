291fb991d5105ae1ee5cb5e63dc5b3688fab77e9
dharsha27 <dharshavardhan27@gmail.com>
2026-06-10 14:44:14 +0530

    changes were made

--- 05-condition.sh
+++ 05-condition.sh
@@ -6,8 +6,10 @@ NUM=$1
 if [ $NUM -ge 20 ]; then  
    echo "The give $NUM is greater than this  number is :20"
 
-else [ $NUM -lt 20 ] ; 
+elif [ $NUM -eq 20 ] ; then
    
-   echo "The give $NUM is less  than this number is :20"
-      
+   echo "The give $NUM is equal to :20"
+else
+    
+      echo "The give $NUM is less than :20"
 fi
