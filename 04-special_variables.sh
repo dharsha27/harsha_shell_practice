69ee0c94acd12bdd355e7daac65b5f8145f2a181
dharsha27 <dharshavardhan27@gmail.com>
2026-06-10 11:57:18 +0530

    I am goint to added to another shell script

---
+++ 04-special_variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "All variables passed on the script.:: $ @"
+echo "Number of variables passed :: $#"
+echo "First variables  :: $1"
+echo "script name :: $0"
+echo "who is the user :: $USER"
+echo "which directory:: $PWD"
+
