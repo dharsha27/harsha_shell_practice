9225b7c9b7f053ba5d02f2af09fc25274bf7788f
dharsha27 <dharshavardhan27@gmail.com>
2026-06-11 15:03:08 +0530

    Changes were made

--- 08-loops.sh
+++ 08-loops.sh
@@ -34,7 +34,7 @@ for package in "$@"
 do 
 
     echo "Installing $package"
-    dnf list installed "$package"
+    dnf list installed "$package" &>> "$LOGS_FILE"
     
     if [ $? -ne 0 ] ; then 
        dnf install "$package" -y &>> "$LOGS_FILE"
