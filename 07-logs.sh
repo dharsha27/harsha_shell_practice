832c6efa896b2bdc53d31d2aa742f5bd1f69ec55
dharsha27 <dharshavardhan27@gmail.com>
2026-06-11 12:53:59 +0530

    Changes were made

--- 07-logs.sh
+++ 07-logs.sh
@@ -7,18 +7,25 @@ USERID=$(id -u)
 LOGS_DIR=/var/log/shell-script 
 LOGS_FILE="$LOGS_DIR/$0.log"
 
+if [ $USERID -ne 0 ] ; then
+   echo "Error: please run this script with root access" | tee -a "$LOGS_FILE"
+   exit 1
+fi
+
+# create a log directory if it doesnt' exist
+mkdir -p /var/log/shell-script 
 
 VALIDATE(){
     if [ $2 -ne 0 ]; then 
           echo "Installing $1 is ....FAILED" | tee -a "$LOGS_FILE"
           exit 1
     else
-          echo "Installing $1 is ....SUCCESS"
+          echo "Installing $1 is ....SUCCESS" | tee -a "$LOGS_FILE"
     fi
 
 }
 
-dnf list installed mysql &>> $LOGS_FILE
+dnf list installed mysql &>> "$LOGS_FILE"
 # echo "Installing my sql"
 if [ $? -eq 0 ]; then 
    echo "MYSQL is Alreaady Installed .. please skip" | tee -a "$LOGS_FILE"
@@ -29,15 +36,15 @@ else
     VALIDATE Mysql $?
 fi
 
+# Nginx Installation
 
-
-dnf list installed nginx &>> $LOGS_FILE
+dnf list installed nginx &>> "$LOGS_FILE"
 echo "Installing my nginx"
 if [ $? -eq 0 ]; then 
    echo "NGINX is Alreaady Installed .. please skip" | tee -a "$LOGS_FILE"
 
 else  
     echo "Installing NGINX " 
-    dnf install nginx -y &>> $LOGS_FILE
-    VALIDATE Mysql $?
+    dnf install nginx -y &>> "$LOGS_FILE"
+    VALIDATE nginx $?
 fi
---
+++ 08-loops.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+
+for num in {1..50}
+do 
+    echo $1
+done
